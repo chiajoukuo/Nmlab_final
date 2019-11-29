461539343bae4b5265b0e3be11064a39babe7a2c
jasoonn <b05901171@ntu.edu.tw>
2019-11-29 18:09:50 +0800

    comment

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import * as serviceWorker from './serviceWorker';
 //modify
 import BlockChainTest from './pages/BlockChainTest';
 
-//ReactDOM.render(<App style={{backgroundColor: 'rgb(36, 33, 33)', height:'100%'}}/>, document.getElementById('root'));
-ReactDOM.render(<BlockChainTest/>, document.getElementById('root'));
+ReactDOM.render(<App style={{backgroundColor: 'rgb(36, 33, 33)', height:'100%'}}/>, document.getElementById('root'));
+//ReactDOM.render(<BlockChainTest/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
