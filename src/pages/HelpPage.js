8dadbd1c3be411a48a04f690990ed04cb9fac510
jasoonn <b05901171@ntu.edu.tw>
2020-01-12 21:57:38 +0800

    update HelpPage

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -23,15 +23,17 @@ class HelpPage extends Component{
                 <h1 className='upload_title'>User Guide</h1>
                 <div className='between'></div>
                 <h3 className='help_topic'>Gallery : </h3>
-                <p className='help_text'>In the gallery, users can browse all other users posts with pictures they creates.</p>
+                <p className='help_text'>In the gallery, users can browse all other users' posts with pictures they creates.</p>
                 <p className='help_text'>Additionally, they can press like and leave comments to interact with other users just like other social media.</p>
                 <p className='help_text'>The best thing here is that users can purchase images they like for other uses such as post on another wall to share with your friends. </p>
                 <h3 className='help_topic'>Posts : </h3>
-                <p className='help_text'></p>
+                <p className='help_text'>In the posts, users can skim through all posts which are based on previously upload picture and perform operations like </p>
                 <h3 className='help_topic'>Post : </h3>
                 <p className='help_text'>Here users can use the images they bought to create posts.</p>
                 <h3 className='help_topic'>Upload : </h3>
-                <p className='help_text'></p>
+                <p className='help_text'>Users can upload images from their devices and use them to create posts. </p>
+                <h3 className='help_topic'>Setting : </h3>
+                <p className='help_text'>In our DApp, accounts are binded to Ethereum accounts. Please don't worry about not setting accounts and passwards. </p>
                 <div className='bgg'></div>
             </>
         );
